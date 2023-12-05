c3467fe19b494d90e41d5b7fc7ff37a7677fcc8c
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-12-05 17:09:30 +0300

    refactor

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -5,24 +5,26 @@ from tests.utils import load_schema
 
 
 def test_get_single_resource_successfully():
-    url = "https://reqres.in/api/unknown/2"
-    schema = load_schema("get_single_resource.json")
+    url = 'https://reqres.in/api/unknown/2'
+    schema = load_schema('get_single_resource.json')
 
     result = requests.get(url)
 
     assert result.status_code == 200
     jsonschema.validate(result.json(), schema)
 
+
 def test_get_single_resource_not_found():
-    url = "https://reqres.in/api/unknown/23"
+    url = 'https://reqres.in/api/unknown/23'
 
     result = requests.get(url)
 
     assert result.status_code == 404
 
+
 def test_get_list_of_resources_schema_successfully():
-    url = "https://reqres.in/api/unknown"
-    schema = load_schema("get_list_resource.json")
+    url = 'https://reqres.in/api/unknown'
+    schema = load_schema('get_list_resource.json')
 
     result = requests.get(url)
 
@@ -31,7 +33,7 @@ def test_get_list_of_resources_schema_successfully():
 
 
 def test_get_list_of_resources_total_pages():
-    url = "https://reqres.in/api/unknown"
+    url = 'https://reqres.in/api/unknown'
     total_pages = 2
 
     result = requests.get(
@@ -39,13 +41,15 @@ def test_get_list_of_resources_total_pages():
         params={"total_pages": total_pages}
     )
 
-    assert result.json()["total_pages"] == total_pages
+    assert result.json()['total_pages'] == total_pages
+
 
 def test_create_new_user_successfully():
-    schema = load_schema("post_create_user.json")
+    url = 'https://reqres.in/api/users'
+    schema = load_schema('post_create_user.json')
 
     result = requests.post(
-        'https://reqres.in/api/users',
+        url,
         {
             "name": "Charles",
             "job": "Ferrari F1 driver"
@@ -57,11 +61,13 @@ def test_create_new_user_successfully():
     assert result.json()['job'] == 'Ferrari F1 driver'
     jsonschema.validate(result.json(), schema)
 
+
 def test_update_existent_user():
-    schema = load_schema("put_update_user.json")
+    url = 'https://reqres.in/api/users/2'
+    schema = load_schema('put_update_user.json')
 
     result = requests.put(
-        'https://reqres.in/api/users/2',
+        url,
         {
             "name": "Charles Leclerc",
             "job": "Ferrari F1 driver"
@@ -72,16 +78,20 @@ def test_update_existent_user():
     assert result.json()['job'] == 'Ferrari F1 driver'
     jsonschema.validate(result.json(), schema)
 
+
 def test_delete_user():
-    result = requests.delete(
-        'https://reqres.in/api/users/2'
-    )
+    url = 'https://reqres.in/api/users/2'
+
+    result = requests.delete(url)
 
     assert result.status_code == 204
 
+
 def test_successful_registration():
+    url = 'https://reqres.in/api/register'
+
     result = requests.post(
-        'https://reqres.in/api/register',
+        url,
         {
             "email": "eve.holt@reqres.in",
             "password": "pistol"
@@ -91,9 +101,12 @@ def test_successful_registration():
     assert result.status_code == 200
     assert result.json()['token'] is not None
 
+
 def test_unsuccessful_registration():
+    url = 'https://reqres.in/api/register'
+
     result = requests.post(
-        'https://reqres.in/api/register',
+        url,
         {
             "email": "eve.holt@reqres.in",
             "password": ""
@@ -103,9 +116,12 @@ def test_unsuccessful_registration():
     assert result.status_code == 400
     assert result.json()['error'] == 'Missing password'
 
+
 def test_successful_login():
+    url = 'https://reqres.in/api/login'
+
     result = requests.post(
-        'https://reqres.in/api/login',
+        url,
         {
             "email": "eve.holt@reqres.in",
             "password": "cityslicka"
@@ -113,11 +129,14 @@ def test_successful_login():
     )
 
     assert result.status_code == 200
-    assert result.json()['token'] == "QpwL5tke4Pnpja7X4"
+    assert result.json()['token'] == 'QpwL5tke4Pnpja7X4'
+
 
 def test_unsuccessful_login():
+    url = 'https://reqres.in/api/login'
+
     result = requests.post(
-        'https://reqres.in/api/login',
+        url,
         {
             "email": "peter@klaven",
             "password": ""
